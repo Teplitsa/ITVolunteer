be72bb346f1fb9bff5495c1408e7cf3176c20f6b
DenisCherniatev <denis.cherniatev@gmail.com>
2015-02-19 12:45:00 +0200

    1

--- tstsite/inc/acf_keys.php
+++ tstsite/inc/acf_keys.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ITV_ACF_TASK_is_tst_consult_needed', ' 	field_54bd335a5078f');
+define('ITV_ACF_TASK_is_tst_consult_needed', 'field_54bd335a5078f');
 define('ITV_ACF_TASK_is_tst_consult_done', 'field_54bd336f50790');
 
 ?>
