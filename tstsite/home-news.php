06ac931052a71d92df49cd74c307ec7f2cab40ba
Anna Ladoshkina <webdev@foralien.com>
2015-03-02 22:17:41 +0300

    Squashed commit of the following:
    commit 86edc9f0861d820f6ebf130d3f1f290ccf417f6b
    Author: Anna Ladoshkina <webdev@foralien.com>
    Date:   Mon Mar 2 22:15:27 2015 +0300
    
        News fixes and consultation email
    
        Fixed incorrect news loop on homepage
        Add email to user when consultation asked on ITV
    
    commit 99eb5ec345a8781880017b26eba9aab2aa319679
    Author: Anna Ladoshkina <webdev@foralien.com>
    Date:   Mon Mar 2 08:51:10 2015 +0300
    
        Squashed commit of the following:
    
        commit e9412823ce82bdb50664f4aacd70df089cea5c20
        Author: DenisCherniatev <denis.cherniatev@gmail.com>
        Date:   Fri Feb 27 11:43:16 2015 +0200
    
            skype lang added
    
        commit ee19099dc99c0154fae5a22a23783ce3a9fc74fa
        Author: DenisCherniatev <denis.cherniatev@gmail.com>
        Date:   Fri Feb 27 11:33:34 2015 +0200
    
            skype to itv profile added
    
        commit 717a9b23ea2a985ea8689f5645e67d4d4c34bab9
        Author: Anna Ladoshkina <webdev@foralien.com>
        Date:   Fri Feb 20 22:50:07 2015 +0300
    
            Squashed commit of the following:
    
            commit 41c020ad223971092082bb4035433cac262f440c
            Author: Anna Ladoshkina <webdev@foralien.com>
            Date:   Fri Feb 20 22:49:19 2015 +0300
    
                fix for https in paging
    
        commit a59aae0ef05f72c9601defb0e2bafd92dd6f7298
        Merge: 2120075 6e127e7
        Author: Anna Ladoshkina <webdev@foralien.com>
        Date:   Fri Feb 20 21:38:13 2015 +0300
    
            Merge branch 'local-dev' into dev
    
    commit 41c020ad223971092082bb4035433cac262f440c
    Author: Anna Ladoshkina <webdev@foralien.com>
    Date:   Fri Feb 20 22:49:19 2015 +0300
    
        fix for https in paging

--- tstsite/home-news.php
+++ tstsite/home-news.php
@@ -10,35 +10,32 @@ $args = array(
 	'suppress_filters' => true 
 );
 
-$news_posts = get_posts( $args );
-$news_post = null;
-if(count($news_posts)) {
-	$news_post = $news_posts[0];
-}
+$news_posts = new WP_Query( $args );
+
 ?>
 
 <?php
-if($news_post):
+if($news_posts->have_posts()): while($news_posts->have_posts()) : $news_posts->the_post();
 ?>
 <article <?php post_class('col-md-6 home-news item-masonry'); ?>>
 <div class="border-card">
 	<h2>Новости проекта</h2>
 	
 	<h4>				
-		<a href="<?php echo post_permalink($news_post->ID); ?>" rel="bookmark"><?php echo get_the_title($news_post->ID); ?></a>
+		<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 	</h4>	
 	
 	<div class="home-news-content">
-		<?$thumbnail = get_the_post_thumbnail($news_post->ID, 'large')?>
+		<?php $thumbnail = get_the_post_thumbnail(get_the_ID(), 'large')?>
 		<?php if ( $thumbnail ): ?>
-			<a href="<?php echo get_permalink($news_post->ID);?>" class="thumb-link"><?php echo $thumbnail ?></a>
+			<a href="<?php the_permalink(); ?>" class="thumb-link"><?php echo $thumbnail ?></a>
 		<?php endif?>
-		<?php global $post; $prev_post = $post; $post = $news_post; ?>
+		
 		<?php echo the_excerpt(); ?>
-		<? $post = $prev_post;?>
+		
 	</div>
 
 </div>
 </article>
 
-<?php endif?>
+<?php endwhile; endif; wp_reset_postdata(); ?>
