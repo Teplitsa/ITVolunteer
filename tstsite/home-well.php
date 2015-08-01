8a273913cb0cdc1cb57182a243d4d728c9e31254
Anna Ladoshkina <webdev@foralien.com>
2015-08-01 15:18:18 +0300

    Home well initial restyling

--- tstsite/home-well.php
+++ tstsite/home-well.php
@@ -1,31 +1,31 @@
 <?php
 /* well on homepage **/
+global $post;
 ?>
 <div class="well">
-	<div class="row">
-		<div class="col-md-2">
-			<div class="intro-logo">
+	
+	<div class="intro-text">
+		<?php echo apply_filters('frl_the_content', $post->post_content);?>
+	</div>
+	
+	<div class="intro-actions">
+		<div class="cta">
 			<?php
-				$src = get_template_directory_uri().'/img/logo-v.png';
-				echo "<img src='{$src}'>";
+				//GA Events
+				$ga_atts = array(
+					'ga_category' => 'Создать задачу',
+					'ga_action' => 'Кнопка создать задачу на главной',
+					'ga_label' => 'Главная'
+				);
 			?>
-			</div>
+				<a href="<?php echo home_url('task-actions');?>" <?php tst_ga_event_data($ga_atts);?> class="btn btn-success ga-event-trigger">
+					<?php _e('Create task', 'tst');?>
+				</a>			
 		</div>
-
-		<div class="col-md-7">
-
-			<div class="home-text"><p>
-            <?php global $post; echo $post->post_content; //the_content(); - not working for some reason ?>
-				<? //_e('it-volunteer - a platform for the exchange of tasks to provide digital services for non-profit organizations and community initiatives. Here you can either ask for help for your project or organization, and to offer it. <a href="http://help.te-st.ru/about/">Read more.</a>', 'tst');?>
-			</p></div>
-
-		</div>
-
-		<div class="col-md-3">
-			<div class="intro-play-video-button pull-right" data-toggle="modal" data-target="#myModal">
-				<div class="intro-play-icon"></div>
-				<div class="intro-play-lable"><?php _e('how it works', 'tst');?></div>
-			</div>
+		
+		<div class="details">
+			<a href="#" data-toggle="modal" data-target="#myModal"><?php _e('how it works', 'tst');?></a>
+			
 			<!-- Modal -->
 			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 				<div class="modal-dialog">
@@ -41,34 +41,21 @@
 				</div>
 			</div>
 		</div>
-	</div><!-- .row -->
-</div>
-
-<div class="home-header-buttons">
-	<div class="row">
-		<div class="col-md-9 clearfix">
-			<div class="home-header-members-count pull-left">
-				<?php _e('Members count', 'tst');?>: <span class="itv-members-count"><?php echo tst_get_active_members_count() ?></span>
-			</div>
-			<div class="itv-tasks-stats pull-left">
-				<span><?php _e('Tasks on site', 'tst');?></span>
-				<a href="<?php echo home_url('/tasks/?st=publish')?>" style="color:#4cae4c;"><?php _e('New tasks:', 'tst')?> <?php echo tst_get_new_tasks_count();?></a>
-				<a href="<?php echo home_url('/tasks/?st=in_work')?>" style="color:#D0021B;"><?php _e('In work tasks:', 'tst')?> <?php echo tst_get_work_tasks_count();?></a>
-				<a href="<?php echo home_url('/tasks/?st=closed')?>"><?php _e('Closed tasks:', 'tst')?> <?php echo tst_get_closed_tasks_count();?></a>
-			</div>
-		</div>
-		<div class="col-md-3">
-		<?php
-			//GA Events
-			$ga_atts = array(
-				'ga_category' => 'Создать задачу',
-				'ga_action' => 'Кнопка создать задачу на главной',
-				'ga_label' => 'Главная'
-			);
-		?>
-			<a href="<?php echo home_url('task-actions');?>" <?php tst_ga_event_data($ga_atts);?> class="add-new-task-button ga-event-trigger">
-				<?php _e('Create task', 'tst');?>
-			</a>
-		</div>
 	</div>
+	
+</div><!-- .well -->	
+
+<div class="home-header-buttons">	
+	
+	<span><?php _e('Tasks on site', 'tst');?></span>
+	<a href="<?php echo home_url('/tasks/?st=publish')?>" class="publish">
+		<?php _e('New tasks:', 'tst')?>&nbsp;<?php echo tst_get_new_tasks_count();?>
+	</a>
+	<a href="<?php echo home_url('/tasks/?st=in_work')?>" class="in_work">
+		<?php _e('In work tasks:', 'tst')?>&nbsp;<?php echo tst_get_work_tasks_count();?>
+	</a>
+	<a href="<?php echo home_url('/tasks/?st=closed')?>" class="closed">
+		<?php _e('Closed tasks:', 'tst')?>&nbsp;<?php echo tst_get_closed_tasks_count();?>
+	</a>	
+	
 </div>
