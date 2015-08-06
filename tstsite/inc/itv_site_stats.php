1f17f717486fead3b3781a9d40f3740027382182
DenisCherniatev <denis.cherniatev@gmail.com>
2015-08-06 20:40:10 +0200

    partial stats collecting added to site stats script

--- tstsite/inc/itv_site_stats.php
+++ tstsite/inc/itv_site_stats.php
@@ -21,53 +21,70 @@ class ItvSiteStats {
 		return $value ? $value : 0;
 	}
 
-	public function refresh_users_role_stats() {
-
-		$users_query_params = array(
-// 			'number' => $per_page,
-// 			'offset' => $offset,
-			'exclude' => ACCOUNT_DELETED_ID,
-		);
-		$user_query = new WP_User_Query($users_query_params);
+	public function refresh_users_role_stats($per_page = 100) {
+		$offset = 0;
 		
-		$USERS_ROLE_BENEFICIARY_COUNT = 0;
-		$USERS_ROLE_SUPERHERO_COUNT = 0;
-		$USERS_ROLE_ACTIVIST_COUNT = 0;
-		$USERS_ROLE_VOLUNTEER_COUNT = 0;
-		$USERS_COUNT = 0;
-				
-		foreach($user_query->results as $user) {
-			$is_count = false;
-			if ( !empty( $user->roles ) && is_array( $user->roles ) ) {
-				foreach ( $user->roles as $role ) {
-					if($role == 'author') {
-						$is_count = true;
+		$is_stop = false;
+		while(!$is_stop) {
+			echo 'offset=' . $offset . "\n";
+			
+			$users_query_params = array(
+				'number' => $per_page,
+				'offset' => $offset,
+				'exclude' => ACCOUNT_DELETED_ID,
+			);
+			$user_query = new WP_User_Query($users_query_params);
+		
+			$USERS_ROLE_BENEFICIARY_COUNT = 0;
+			$USERS_ROLE_SUPERHERO_COUNT = 0;
+			$USERS_ROLE_ACTIVIST_COUNT = 0;
+			$USERS_ROLE_VOLUNTEER_COUNT = 0;
+			$USERS_COUNT = 0;
+			
+			$users_count_portion = 0;
+					
+			foreach($user_query->results as $user) {
+				$is_count = false;
+				if ( !empty( $user->roles ) && is_array( $user->roles ) ) {
+					foreach ( $user->roles as $role ) {
+						if($role == 'author') {
+							$is_count = true;
+						}
 					}
 				}
-			}
 						
-			if($is_count) {
+				if($is_count) {
+					
+					tst_update_member_stat($user);
+					$user_role = tst_get_member_role_key($user);
+					
+					if($user_role == 'donee') {
+						$USERS_ROLE_BENEFICIARY_COUNT += 1;
+					}
+					elseif($user_role == 'hero') {
+						$USERS_ROLE_SUPERHERO_COUNT += 1;
+					}
+					elseif($user_role == 'activist') {
+						$USERS_ROLE_ACTIVIST_COUNT += 1;
+					}
+					elseif($user_role == 'volunteer') {
+						$USERS_ROLE_VOLUNTEER_COUNT += 1;
+					}
+					else {
+						$USERS_COUNT +=1;
+					}
+				}
 				
-				tst_update_member_stat($user);
-				$user_role = tst_get_member_role_key($user);
-				
-				if($user_role == 'donee') {
-					$USERS_ROLE_BENEFICIARY_COUNT += 1;
-				}
-				elseif($user_role == 'hero') {
-					$USERS_ROLE_SUPERHERO_COUNT += 1;
-				}
-				elseif($user_role == 'activist') {
-					$USERS_ROLE_ACTIVIST_COUNT += 1;
-				}
-				elseif($user_role == 'volunteer') {
-					$USERS_ROLE_VOLUNTEER_COUNT += 1;
-				}
-				else {
-					$USERS_COUNT +=1;
-				}
+				$users_count_portion += 1;
 			}
+			
+			if($users_count_portion < $per_page) {
+				$is_stop = true;
+			}
+			
+			$offset += $per_page;
 		}
+		
 		
 		$USERS_COUNT += ($USERS_ROLE_BENEFICIARY_COUNT+$USERS_ROLE_SUPERHERO_COUNT+$USERS_ROLE_ACTIVIST_COUNT+$USERS_ROLE_VOLUNTEER_COUNT);
 		
