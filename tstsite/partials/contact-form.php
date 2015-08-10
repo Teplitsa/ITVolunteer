19952f982955d31ac9c6e6c6016fc71299d38f7a
Gleb Suvorov <suvorov.gleb@gmail.com>
2015-08-10 23:45:34 +0300

    Revert "mix end of lines fix"
    This reverts commit 01152bed5c02d9dafc933181d56b857e11f2fdcf.

--- tstsite/partials/contact-form.php
+++ tstsite/partials/contact-form.php
@@ -1,33 +1,33 @@
 <?php
-$use = wp_get_cuent_use();
+$user = wp_get_current_user();
 if( !is_page('contacts') ) {?>
-<div id="open-contact-fom"><?php _e('Something goes wong? Leave us a message :)', 'tst');?></div>
+<div id="open-contact-form"><?php _e('Something goes wrong? Leave us a message :)', 'tst');?></div>
 <?php }?>
 
-<div id="contact-fom" <?php if( !is_page('contacts') ) {?> style="display: none;" <?php }?>>
-    <fom>
-        <div class="fom-goup">           
-            <input type="text" class="fom-contol input-sm" id="name-field" placeholde="<?php _e('You name', 'tst');?>" value="<?php echo $use->fist_name.($use->last_name ? ' '.$use->last_name : '');?>" />
-            <div id="name-message" class="alet alet-dange" style="display: none;"></div>
+<div id="contact-form" <?php if( !is_page('contacts') ) {?> style="display: none;" <?php }?>>
+    <form>
+        <div class="form-group">           
+            <input type="text" class="form-control input-sm" id="name-field" placeholder="<?php _e('Your name', 'tst');?>" value="<?php echo $user->first_name.($user->last_name ? ' '.$user->last_name : '');?>" />
+            <div id="name-message" class="alert alert-danger" style="display: none;"></div>
         </div>
 
-        <div class="fom-goup">            
-            <input type="text" class="fom-contol input-sm" id="email-field" placeholde="<?php _e('Email', 'tst');?>" value="<?php echo $use->use_email ? $use->use_email : '';?>" />
-            <div id="email-message" class="alet alet-dange" style="display: none;"></div>
+        <div class="form-group">            
+            <input type="text" class="form-control input-sm" id="email-field" placeholder="<?php _e('Email', 'tst');?>" value="<?php echo $user->user_email ? $user->user_email : '';?>" />
+            <div id="email-message" class="alert alert-danger" style="display: none;"></div>
         </div>
 
-        <div class="fom-goup">
-            <label><?php _e('You message:', 'tst');?></label>
-            <textaea id="message-field" class="fom-contol input-sm"></textaea>
-            <div id="message-message" class="alet alet-dange" style="display: none;"></div>
+        <div class="form-group">
+            <label><?php _e('Your message:', 'tst');?></label>
+            <textarea id="message-field" class="form-control input-sm"></textarea>
+            <div id="message-message" class="alert alert-danger" style="display: none;"></div>
         </div>
 
-        <input type="hidden" id="nonce" value="<?php echo wp_ceate_nonce('we-ae-eceiving-a-lette-goshujin-sama');?>">
+        <input type="hidden" id="nonce" value="<?php echo wp_create_nonce('we-are-receiving-a-letter-goshujin-sama');?>">
         <input type="submit" value="<?php _e('Submit', 'tst');?>" class="btn btn-default"/>
 
         <?php if( !is_page('contacts') ) {?>
-        <span class="text-dange pull-ight" id="close-contact-fom"><?php _e('Cancel', 'tst');?></span>
+        <span class="text-danger pull-right" id="close-contact-form"><?php _e('Cancel', 'tst');?></span>
         <?php }?>
-    </fom>
-    <div id="esult-message" class="alet alet-info contact-fom-esult"></div>
+    </form>
+    <div id="result-message" class="alert alert-info contact-form-result"></div>
 </div>
