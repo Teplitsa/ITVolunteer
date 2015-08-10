3f631a3a2f73b12b7a2d9683291f62509cbe83e7
DenisCherniatev <denis.cherniatev@gmail.com>
2015-08-10 11:13:17 +0200

    version fix

--- tstsite/functions.php
+++ tstsite/functions.php
@@ -1,5 +1,5 @@
 <?php
-define('TST_WORKING_VERSION', '1.9.4');
+define('TST_WORKING_VERSION', '1.9.5');
 @error_reporting(E_ALL & ~E_NOTICE);
 require get_template_directory().'/inc/acf_keys.php';
 
