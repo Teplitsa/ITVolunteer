51d4b189fbf27367cb07313f8acdfd76034226b7
Anna Ladoshkina <webdev@foralien.com>
2015-08-10 22:27:40 +0300

    EOF fix

--- tstsite/inc/user_profile.php
+++ tstsite/inc/user_profile.php
@@ -215,17 +215,6 @@ function tst_show_user_skills($skills) {
 	}
 }
 
-function tst_get_member_user_skills_string($member_id) {
-	$user_skills = tst_get_user_skills($member_id);
-	$actual_user_skills = array();
-	foreach($user_skills as $skill) {
-		if($skill['checked']) {
-			$actual_user_skills[] = $skill['cat_name'];
-		}
-	}
-	return implode(', ', $actual_user_skills);
-}
-
 // SEO Title 
 add_filter( 'wpseo_title', 'itv_user_profile_seo_title');
 function itv_user_profile_seo_title($title) {
@@ -244,4 +233,15 @@ function itv_user_profile_seo_title($title) {
 	}
 	
 	return $title;
+}
+
+function tst_get_member_user_skills_string($member_id) {
+	$user_skills = tst_get_user_skills($member_id);
+	$actual_user_skills = array();
+	foreach($user_skills as $skill) {
+		if($skill['checked']) {
+			$actual_user_skills[] = $skill['cat_name'];
+		}
+	}
+	return implode(', ', $actual_user_skills);
 }
