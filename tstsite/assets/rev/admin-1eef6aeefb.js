307b914c6e94c18db7359dac77a788ea5dbd5d53
Anna Ladoshkina <webdev@foralien.com>
2015-09-03 10:41:01 +0300

    revision folder

---
+++ tstsite/assets/rev/admin-1eef6aeefb.js
@@ -0,0 +1,28 @@
+/* scripts */
+
+(function($) {
+ 
+	$(document).ready(function(){
+        $('#save-task-status').click(function(e){
+            e.preventDefault();
+						
+            $('#post-status-display').html($('#post_status option:selected').html());
+        });
+	
+	var is_tst_consult_needed_init_val = $('#acf-field-is_tst_consult_needed-1').prop('checked');
+	$('#acf-field-is_tst_consult_needed-1').change(function(){
+		$('#acf-field-is_tst_consult_needed-1').prop('checked', is_tst_consult_needed_init_val);
+	});	
+	
+	if($('#acf-field-is_tst_consult_needed-1').prop('checked') == false) {
+		$('div#acf-is_tst_consult_done').hide();
+	}
+	else {
+		var tst_logo = $('<img src="'+adminend.site_url+'/wp-content/itv/tstsite/assets/img/te-st-logo.jpg" />');
+		tst_logo.css({'vertical-align': 'middle'});
+		$('div#acf-is_tst_consult_needed li').append(tst_logo);
+	}
+    });
+	
+	
+})(jQuery);
