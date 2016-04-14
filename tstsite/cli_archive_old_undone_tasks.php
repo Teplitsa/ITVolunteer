f51b2569df0cf77676cea1eaa91582296f4dee8f
DenisCherniatev <denis.cherniatev@gmail.com>
2016-04-14 12:17:30 +0200

    debug info added

--- tstsite/cli_archive_old_undone_tasks.php
+++ tstsite/cli_archive_old_undone_tasks.php
@@ -41,14 +41,21 @@ try {
 	
 	$sql = "SELECT time_add FROM str_sent_notifications WHERE notif_type = %s ORDER BY time_add ASC LIMIT 1";
 	$first_notif_dt = $wpdb->get_var($wpdb->prepare($sql, $notif_key));
+	echo 'first_long_work_notif_dt=' . $first_notif_dt . "\n";
+	$is_time_to_archive_long_work = false;
 	if($first_notif_dt) {
 	    $first_notif_time = strtotime($first_notif_dt);
 	    $first_notif_time_start = $first_notif_time + 24 * 3600 * ($itv_config->get('TASK_LONG_WORK_ARCHIVE_DAYS') - $itv_config->get('TASK_LONG_WORK_NOTIF_DAYS'));
 	    $now = time();
 	    // wait sometime after first notifications sent
 	    if($now > $first_notif_time_start) {
+	        $is_time_to_archive_long_work = true;
+	        echo "TIME to archive long work tasks...\n";
 	        $itv_archiver->archive_tasks($action_key, $task_status, $task_archive_days, ItvLog::$ACTION_TASK_LONG_WORK_ARCHIVE, $is_check_doers);
 	    }
+	}
+	if(!$is_time_to_archive_long_work) {
+	    echo "NOT TIME to archive long work tasks\n";
 	}
 	
 	echo_end_text();
