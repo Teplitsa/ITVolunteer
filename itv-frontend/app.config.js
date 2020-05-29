a8fdeb46ca693a065f64cc58d17329f9d74a778f
alekseyesev <alekseyesev@live.ru>
2020-05-29 19:01:43 +0300

    Added differentiation of app constants depending on runtime

--- itv-frontend/app.config.js
+++ itv-frontend/app.config.js
@@ -1,6 +1,14 @@
 const appConfig = {
-  GraphQLServer: "https://itv.ngo2.ru/graphql",
-  AjaxUrl: "https://itv.ngo2.ru/wp-admin/admin-ajax.php",
+  GraphQLServer: `${
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:9000"
+      : "https://itv.ngo2.ru"
+  }/graphql`,
+  AjaxUrl: `${
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:9000"
+      : "https://itv.ngo2.ru"
+  }/wp-admin/admin-ajax.php`,
   AuthTokenLifeTimeMs: 600,
 };
 
