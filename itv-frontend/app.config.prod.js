61ab9380ea3c9fccaa32aebeb41ad0d41963e8b5
DenisCherniatev <denis.cherniatev@gmail.com>
2020-06-23 06:25:55 +0200

    * custom config added

---
+++ itv-frontend/app.config.prod.js
@@ -0,0 +1,7 @@
+const appConfig = {
+  GraphQLServer: "https://itv.te-st.ru/graphql",
+  AjaxUrl: "https://itv.te-st.ru/wp-admin/admin-ajax.php",
+  AuthTokenLifeTimeMs: 600,
+};
+
+module.exports = appConfig;
