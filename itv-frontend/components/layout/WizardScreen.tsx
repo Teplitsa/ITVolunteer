02892e4c80abcde6091e809a1723d4cd66d8388a
DenisCherniatev <denis.cherniatev@gmail.com>
2020-07-04 21:06:07 +0200

    * wizard updated

--- itv-frontend/components/layout/WizardScreen.tsx
+++ itv-frontend/components/layout/WizardScreen.tsx
@@ -1,8 +1,8 @@
 import { ReactElement, useState, useEffect, useRef } from "react";
 import { useStoreState } from "../../model/helpers/hooks";
 
-import OrnamentImage from "../../assets/img/bg-modal-ornament.svg";
-import Logo from "../../assets/img/pic-logo-itv.svg";
+import logo from "../../assets/img/pic-logo-itv.svg";
+import howToIcon from "../../assets/img/icon-question-green.svg";
 
 const WizardScreen: React.FunctionComponent<{
   goNextStep,
@@ -14,7 +14,7 @@ const WizardScreen: React.FunctionComponent<{
       <div className="wizard__container wizard__ornament">
 
         <header>
-          <img src={Logo} className="wizard__logo" alt="IT-волонтер" />
+          <img src={logo} className="wizard__logo" alt="IT-волонтер" />
         </header>
 
         <div className="wizard__content">
@@ -26,6 +26,7 @@ const WizardScreen: React.FunctionComponent<{
   );
 
 };
+
 
 export const WizardScreenBottomBar = (props) => {
 
@@ -41,27 +42,64 @@ export const WizardScreenBottomBar = (props) => {
 
   return (
     <div className="wizard-bottom-bar">
-      <div className="wizard-bottom-bar__title">
-        {!!props.icon && 
-          <img src={props.icon} alt="icon" />
-        }        
-        <span>{props.title}</span>
-      </div>
-      <div>
-        <a href="#" onClick={handleNextClick}>Далее</a>
-        <br />
-        <a href="#" onClick={handlePrevClick}>Назад</a>
-      </div>
-      <div className="wizard-progressbar">
-        <div className="wizard-progressbar__fraction">{`${props.step + 1}/${props.stepsCount}`}</div>
-        <div className="wizard-progressbar__total">
-          <div className="wizard-progressbar__complete" style={{
-            width: `${100 * (props.step + 1) / props.stepsCount}%`,
-          }}></div>
+      <div className="wizard-bottom-bar__container">
+        <div className="wizard-bottom-bar__title">
+          {!!props.icon && 
+            <img src={props.icon} alt="icon" />
+          }        
+          <span>{props.title}</span>
+        </div>
+        <div>
+          <a href="#" onClick={handleNextClick}>Далее</a>
+          <br />
+          <a href="#" onClick={handlePrevClick}>Назад</a>
+        </div>
+        <div className="wizard-progressbar">
+          <div className="wizard-progressbar__fraction">{`${props.step + 1}/${props.stepsCount}`}</div>
+          <div className="wizard-progressbar__total">
+            <div className="wizard-progressbar__complete" style={{
+              width: `${100 * (props.step + 1) / props.stepsCount}%`,
+            }}></div>
+          </div>
         </div>
       </div>
     </div>
   )
 }
+
+export const WizardLimitedTextField = ({children, ...props}) => {
+  return (
+    <div className="wizard-field">
+      {children}
+      <div className="wizard-field__limit-help">
+        <div className="wizard-field__help">
+          <img src={howToIcon} className="wizard-field__icon" />
+          {props.howtoTitle}
+        </div>
+        <div className="wizard-field__limit">{`1/${props.maxLength}`}</div>
+      </div>
+    </div>
+  )
+}
+
+
+export const WizardStringField = (props) => {
+  return (
+    <WizardLimitedTextField {...props}>
+      <input type="text" placeholder={props.placeholder} />
+    </WizardLimitedTextField>
+  )
+}
+
+
+export const WizardTextField = (props) => {
+  return (
+    <WizardLimitedTextField {...props}>
+      <textarea placeholder={props.placeholder}></textarea>
+    </WizardLimitedTextField>
+  )
+}
+
+
 
 export default WizardScreen;
--- itv-frontend/components/task-actions/CreateTaskScreens.tsx
+++ itv-frontend/components/task-actions/CreateTaskScreens.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { useStoreState } from "../../model/helpers/hooks";
-import { WizardScreenBottomBar } from "../layout/WizardScreen";
+import { WizardScreenBottomBar, WizardStringField, WizardTextField } from "../layout/WizardScreen";
 import BottomIcon from "../../assets/img/icon-task-list-gray.svg";
 
 
@@ -8,10 +8,7 @@ export const AgreementScreen = (props) => {
 
   return (
     <div className="wizard-screen agreement">
-      <div className="wizard-form">
-        <h1>Что должен знать автор задачи перед ее постановкой</h1>
-      </div>
-
+      <h1>Что должен знать автор задачи перед ее постановкой</h1>
       <WizardScreenBottomBar {...props} icon={BottomIcon} title="Создание задачи" />
     </div>
   );
@@ -29,6 +26,11 @@ export const SetTaskTitleScreen = (props) => {
           <span>{props.step + 1} →</span>
           Название задачи <span className="wizard-form__required-star">*</span>
         </h1>
+        <WizardStringField {...props} 
+          placeholder="Например, «Разместить счётчик на сайте»" 
+          howtoTitle="Как правильно дать название задачи" 
+          maxLength={50}
+        />
       </div>
 
       <WizardScreenBottomBar {...props} icon={BottomIcon} title="Создание задачи" />
@@ -43,7 +45,15 @@ export const SetTaskDescriptionScreen = (props) => {
     <div className="wizard-screen">
 
       <div className="wizard-form">
-        <h1>Опишите, что нужно сделать *</h1>
+        <h1>
+          <span>{props.step + 1} →</span>
+          Опишите, что нужно сделать <span className="wizard-form__required-star">*</span>
+        </h1>
+        <WizardTextField {...props} 
+          placeholder="Какая задача стоит перед IT-волонтером?" 
+          howtoTitle="Как правильно дать название задачи" 
+          maxLength={250}
+        />
       </div>
 
       <WizardScreenBottomBar {...props} icon={BottomIcon} title="Создание задачи" />
