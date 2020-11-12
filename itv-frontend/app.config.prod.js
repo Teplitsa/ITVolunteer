33e51c1a7c7d339bd66b9aae4683ad65f324b443
DenisCherniatev <denis.cherniatev@gmail.com>
2020-11-12 21:28:16 +0200

    * fix prod config

--- itv-frontend/app.config.prod.js
+++ itv-frontend/app.config.prod.js
@@ -1,6 +1,7 @@
 const appConfig = {
   GraphQLServer: "https://itv.te-st.ru/graphql",
   AjaxUrl: "https://itv.te-st.ru/wp-admin/admin-ajax.php",
+  RestApiUrl: "https://itv.te-st.ru/wp-json",
   AuthTokenLifeTimeMs: 600,
 };
 
