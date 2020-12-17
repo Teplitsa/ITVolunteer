309ac969125d5667cd1bceb9e9ff98a16cd2b99d
alekseyesev <alekseyesev@live.ru>
2020-12-17 17:58:29 +0300

    Fix auth bugs

--- itv-frontend/pages/members/[username]/[portfolio_item_slug]/edit.tsx
+++ itv-frontend/pages/members/[username]/[portfolio_item_slug]/edit.tsx
@@ -98,16 +98,18 @@ export const getServerSideProps: GetServerSideProps = async ({ query, req, res }
     },
   });
 
-  const loggedIn = decodeURIComponent(req.headers.cookie).match(/wordpress_logged_in_[^=]+=([^|]+)/);
+  const loggedIn = decodeURIComponent(req.headers.cookie).match(
+    /wordpress_logged_in_[^=]+=([^|]+)/
+  );
 
-  if (loggedIn && (query.username as string).toLowerCase() === loggedIn[1].toLowerCase()) {
-    return {
-      props: { ...model },
-    };
-  } else {
+  if (!loggedIn /* || (query.username as string).toLowerCase() !== loggedIn[1].toLowerCase()*/) {
     res.statusCode = 401;
     Object.assign(model, { statusCode: 401 });
   }
+
+  return {
+    props: { ...model },
+  };
 };
 
 export default PortfolioItemPage;
