b1aa90187eb1c2b1bf31be7637ceb4288df3b0be
Алексей Есев <alekseyesev@live.ru>
2021-04-28 17:23:15 +0300

    Fix faq more link

--- itv-frontend/components/home/HomeFAQ.tsx
+++ itv-frontend/components/home/HomeFAQ.tsx
@@ -38,11 +38,9 @@ const HomeFAQAccordionAuthor: React.FunctionComponent<{ authorFaqs: Array<IFaq> 
       {authorFaqs.map(({ _id, title, content }) => {
         return (
           <div key={`HomeFaqItem-${_id}`} data-accordion-item className="home-faq__item">
-            <div
-              data-accordion-title
-              className="home-faq__item-title"
-              dangerouslySetInnerHTML={{ __html: title }}
-            />
+            <div data-accordion-title className="home-faq__item-title">
+              <div data-accordion-control dangerouslySetInnerHTML={{ __html: title }} />
+            </div>
             <div
               data-accordion-content
               className="home-faq__item-content"
@@ -61,9 +59,10 @@ const HomeFAQ: React.FunctionComponent = (): ReactElement => {
   const template = useStoreState(state => state.components.homePage.template);
   const faqList = useStoreState(state => state.components.homePage.faqList);
 
-  const faqs = isFullListShown
-    ? faqList.filter(({ userRole }) => userRole === template)
-    : faqList.filter(({ userRole }) => userRole === template).slice(0, MAX_FAQ_COUNT_TO_PREVIEW);
+  let faqs = faqList.filter(({ userRole }) => userRole === template);
+  const maxFaqCount = faqs.length;
+
+  faqs = isFullListShown ? faqs : faqs.slice(0, MAX_FAQ_COUNT_TO_PREVIEW);
 
   const HomeFAQAccordion: React.FunctionComponent = (): ReactElement =>
     (template === "author" && <HomeFAQAccordionAuthor {...{ authorFaqs: faqs }} />) || (
@@ -82,10 +81,14 @@ const HomeFAQ: React.FunctionComponent = (): ReactElement => {
 
   return (
     <section className="home-faq">
-      <h3 className="home-faq__title home-title">Остались вопросы?</h3>
+      <h3 className="home-faq__title home-title">
+        Остались вопросы?
+        <br />
+        Сейчас всё расскажем!
+      </h3>
       <HomeInterfaceSwitch />
       <HomeFAQAccordion />
-      {faqs.length > MAX_FAQ_COUNT_TO_PREVIEW && !isFullListShown && (
+      {!isFullListShown && maxFaqCount > 3 && (
         <button className="home-faq__more btn btn_link" type="button" onClick={handleShowFullList}>
           Все вопросы
         </button>
