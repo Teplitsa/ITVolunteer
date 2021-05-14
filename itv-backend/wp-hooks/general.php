4742d329bec2deab51234df4205f0411d69f5309
DenisCherniatev <denis.cherniatev@gmail.com>
2021-05-14 20:00:01 +0200

    fix

--- itv-backend/wp-hooks/general.php
+++ itv-backend/wp-hooks/general.php
@@ -2,7 +2,7 @@
 
 // filters
 function itv_prefix_disable_gutenberg(bool $current_status, string $post_type) {
-    return ($post_type === ITV\models\Task::POST_TYPE) ? false : $current_status;
+    return ($post_type === \ITV\models\Task::POST_TYPE) ? false : $current_status;
 }
 add_filter('use_block_editor_for_post_type', 'itv_prefix_disable_gutenberg', 10, 2);
 
