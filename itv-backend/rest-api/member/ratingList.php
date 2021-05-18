ae1f708703656ecae23f5e4105f0cf9dfa892a02
DenisCherniatev <denis.cherniatev@gmail.com>
2021-05-18 16:40:12 +0200

    fix

--- itv-backend/rest-api/member/ratingList.php
+++ itv-backend/rest-api/member/ratingList.php
@@ -89,7 +89,7 @@ class RatingLIst {
         $sql_group_by = " GROUP BY {$wpdb->users}.ID ";
     
         $args['custom_sql'] = call_user_func_array([$wpdb, 'prepare'], array_merge([$sql_select . $sql_from . $sql_where . $sql_group_by], $args_from, $args_where));    
-        $args['custom_sql_order'] = " HAVING solved_tasks_count > 0 AND solved_tasks_position > 0 ORDER BY solved_tasks_count DESC, {$wpdb->users}.ID ASC";
+        $args['custom_sql_order'] = " HAVING solved_tasks_count > 0 AND solved_tasks_position > 0 ORDER BY solved_tasks_position ASC";
         $args['count_total'] = true;
     
         return $args;
