37af21fa8ee6964a51a301f9924202295337b8f6
DenisCherniatev <denis.cherniatev@gmail.com>
2021-06-16 07:53:09 +0200

    fix dev config

--- itv-backend/config.dev.php
+++ itv-backend/config.dev.php
@@ -3,10 +3,9 @@
 namespace ITV;
 
 class Config {
-    const MONGO_CONNECTION = 'mongodb://mongo:27017';
+    const MONGO_CONNECTION = 'mongodb://localhost:27017';
 
     const AUTH_EXPIRE_DAYS = 30;
 
     const MAIL_ABOUT_TASKS_SKIP_LIST = ['sidorenko.a@te-st.ru'];
 }
-
