d4a251ed3964cdbb56a710c8df43e8a8ac649678
DenisCherniatev <denis.cherniatev@gmail.com>
2021-06-28 14:47:49 +0200

    fix Telegram message

--- itv-backend/models/telegram.php
+++ itv-backend/models/telegram.php
@@ -18,7 +18,7 @@ class Telegram
 
         $link = get_permalink( $task );
 
-        $message = "<b>{$title}</b>\n{$content}\n{$link}";
+        $message = "<b>{$title}</b>\n{$content}\n{$link}\n\n";
 
         // print("message: " . $message);
 
