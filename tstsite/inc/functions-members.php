1ebcab194a46a0fee6241f9d0ee0918a199c7c67
DenisCherniatev <denis.cherniatev@gmail.com>
2021-08-04 14:59:37 +0200

    fix

--- tstsite/inc/functions-members.php
+++ tstsite/inc/functions-members.php
@@ -1833,7 +1833,7 @@ function itv_get_user_approved_as_doer_tasks($user_id) {
 }
 
 function itv_is_empty_user_profile($user_id) {
-    $portfolio = PortfolioWorkManager();
+    $portfolio = new PortfolioWorkManager();
     return (count(itv_get_user_created_tasks($user_id)) + count(itv_get_user_approved_as_doer_tasks($user_id)) === 0) && $portfolio->count_member_portfolio_works($user_id) === 0;
 }
 
